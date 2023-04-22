9d3d0cb8cce89cc4cc2a02cd0a7d0d04912079eb
kienngynh <kien.ngynh@example.com>
2023-04-22 17:22:13 +0700

    quick update

--- rust/eamontech-course/part2-special-features/src/main.rs
+++ rust/eamontech-course/part2-special-features/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    fn _x() {
+        let _z = "hello";
+        let _w = 100;
+        _y();
+    }
+
+    fn _y() {
+        let mut _s = "world".to_owned();
+        _s.push_str("!");
+    }
+
+    let mut _s1 = "hello".to_owned();
+    let _s2 = &_s1;
+    println!("{}", _s2);
 }
